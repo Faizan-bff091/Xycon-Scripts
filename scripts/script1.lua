ee518acf3fe16546c499ff43004f481cb0c49019
Faizan-bff091 <faizanaliirfan99@gmail.com>
2025-05-10 19:07:39 +0100

    Add files via upload

---
+++ scripts/script1.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xycon-developer/Xycon-AOT-script/refs/heads/main/Script"))()
+
+Join Our Discord :https://discord.gg/jCvZZwy3Vz
---
+++ scripts/script2.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xycon-developer/Xycon-Lucky-battlegrounds-script/main/Script"))()
+
+Join Our Discord : https://discord.gg/jCvZZwy3Vz
